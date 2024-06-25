e097f3b280c06d701768087f13b7d149c7e8cd20
VasundharaShivankar <vasundharashivankar179@gmail.com>
2024-06-25 23:12:56 +0530

    blog views created

--- Desktop/blogapp2/src/index.js
+++ Desktop/blogapp2/src/index.js
@@ -11,7 +11,7 @@ const router = createBrowserRouter([
     element: <Home />
   },
   {
-    path: "/blog",
+    path: "/blog/:id",
     element: <BlogView />
   }
 ])
--- Desktop/blogapp2/src/views/BlogView/BlogView.js
+++ Desktop/blogapp2/src/views/BlogView/BlogView.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import "./BlogView.css"
+import { useParams } from 'react-router-dom'
+import blogs from "./../../data"
 
 function BlogView() {
+    const {id}= useParams()
+
+    const selectedBlog=blogs.find((blogData)=>{
+        if(blogData.id==id){
+            return true
+        }
+        else{
+            return false
+        }
+    })
+    console.log(selectedBlog)
   return (
     <div>
-      
+      <h1>Blog View:{id}</h1>
     </div>
   )
 }
