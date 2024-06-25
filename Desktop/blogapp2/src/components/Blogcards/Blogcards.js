2b580747aa3e8722f5045f716ca1e9dc58f222e8
VasundharaShivankar <vasundharashivankar179@gmail.com>
2024-06-25 21:15:06 +0530

    categories added

--- Desktop/blogapp2/src/components/Blogcards/Blogcards.js
+++ Desktop/blogapp2/src/components/Blogcards/Blogcards.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import "./Blogcards.css"
 
-function Blogcards({title,content,author, data,categories}) {
+function Blogcards({title,content,author, date,categories}) {
   return (
     <div className="blog-cards">
       <h1 className="blog-title">{title}</h1>
       <p className="blog-content">{content.substring(0,120)}...</p>
       <div className="author"> 
         <img src={author.avatar} alt="author-img" className="author-avatar"/>
-        <span className='author-name'>{author.name}</span>
-        
+        <span className='author-name'>{author.name}<p className="blog-date">{date}</p></span>
       </div>
+      {categories.map((category,i)=>{
+        return <span key={i} className='blog-category'>{category} </span>
+      })}
     </div>
   )
 }
