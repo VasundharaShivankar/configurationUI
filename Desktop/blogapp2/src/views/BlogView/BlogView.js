d4a1743273d21c8e4b90bd9d9a663a4e36c8ff27
VasundharaShivankar <vasundharashivankar179@gmail.com>
2024-06-26 00:32:37 +0530

    blog page created with blog views

--- Desktop/blogapp2/src/views/BlogView/BlogView.js
+++ Desktop/blogapp2/src/views/BlogView/BlogView.js
@@ -4,22 +4,21 @@ import { useParams } from 'react-router-dom'
 import blogs from "./../../data"
 
 function BlogView() {
-    const {id}= useParams()
-
-    const selectedBlog=blogs.find((blogData)=>{
-        if(blogData.id==id){
-            return true
-        }
-        else{
-            return false
-        }
-    })
-    console.log(selectedBlog)
-  return (
-    <div>
-      <h1>Blog View:{id}</h1>
-    </div>
-  )
+    const { id } = useParams()
+    const selectedBlog = blogs.find((blogData) => blogData.id == id)
+    return (
+        <div className='blogView'>
+            <h1 className='blogView-title'>{selectedBlog.title}</h1>
+            <img src={selectedBlog.image} alt="blog-img" className="blog-img" />
+           <div  className='blogView-author'>
+            <p>
+                <img src={selectedBlog.author.avatar} alt="author" className="author-avatar" /></p>
+            <p>{selectedBlog.author.name} | {selectedBlog.date}</p>   
+            </div>
+            <p className='blogView-content'>{selectedBlog.content}</p>
+        </div>
+    )
 }
 
 export default BlogView
+
