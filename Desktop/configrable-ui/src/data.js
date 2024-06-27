70bf3b064476eb0dccb97464311cd11800af5009
VasundharaShivankar <vasundharashivankar179@gmail.com>
2024-06-27 22:29:36 +0530

    initial commit

---
+++ Desktop/configrable-ui/src/data.js
@@ -0,0 +1,31 @@
+const siteDetails = {
+    name: "Sri Venkateswara College of Engineering",
+    address: "Sri Venkateswara College of Engineering, Sriperumbudur, Kancheepuram District, Tamil Nadu, India",
+}
+
+
+const birthdayCakes = [
+    {
+        img: "https://www.fnp.com/gift/butterscotch-birthday-cake-half-kg?OCCASION_TAGS=birthday&pos=7",
+        title: "Butterscotch Birthday Cake- Half Kg",
+        price: "₹ 799",
+        description: "Indulge them in the decadent delight of our butterscotch cake, crafted to elevate celebrations with its rich, caramelised flavours. Perfect for those who savour life's sweetness, this cake is a symphony of buttery notes and golden caramel, making every bite an exquisite treat. Gift this to add a touch of joy to any occasion.",
+        productDetails: {
+            Flavour: "Butter scotch",
+            weight: "Half Kg",
+            cakeVersion: "Contains egg",
+            typeOfCake: "Cream cake",
+            shape: "Round",
+            serves: " 4-6 people",
+            size: "6 inches (approx)",
+            netQuantity: "1 Unit"
+        }
+    }
+]
+
+const anniversaryCakes = [
+    {
+
+    }
+]
+export { siteDetails }
---
+++ Desktop/configrable-ui/src/views/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { siteDetails } from './../../data'
+
+function Home() {
+  return (
+    <div>
+     <h1>{siteDetails.name}</h1>
+    </div>
+  )
+}
+
+export default Home
